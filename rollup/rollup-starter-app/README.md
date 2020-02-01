22ad590167cbed5065c2cea9034a47c9b3b69ca1
aenrathiel <54458919+aenrathiel@users.noreply.github.com>
2020-01-31 21:00:03 -0600

    Checkpoint 1 files
    Final Push

---
+++ rollup/rollup-starter-app/README.md
@@ -0,0 +1,6 @@
+Checkpoint 1
+
+npm run build builds the application to public/bundle.js
+
+npm start launches a local development server
+
---
+++ rollup/rollup-starter-app/public/bundle.js
@@ -0,0 +1,2 @@
+!function(){"use strict";var e=document.querySelector("#message");console.log("if you have sourcemaps enabled in your devtools, click on main.js:5 --\x3e"),e.textContent="Hello World"}();
+//# sourceMappingURL=bundle.js.map
